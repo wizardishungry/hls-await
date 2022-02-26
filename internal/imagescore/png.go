07b1da1f90e1dd17579c0b4f18a181fcca255eb3
Jon Williams <jon@jonwillia.ms>
2022-02-26 17:52:52 -0500

    use a png bufferpool pinned to a single goroutine

--- internal/imagescore/png.go
+++ internal/imagescore/png.go
@@ -7,17 +7,52 @@ import (
 	"sync"
 )
 
-type PngScorer struct{}
+type PngScorer struct {
+	enc png.Encoder
+}
 
 var _ ImageScorer = &PngScorer{}
 
-func NewPngScorer() *PngScorer { return &PngScorer{} }
+func NewPngScorer() *PngScorer {
+	return &PngScorer{
+		enc: png.Encoder{
+			CompressionLevel: png.BestSpeed,
+			BufferPool:       &singleThreadBufferPool{},
+		},
+	}
+}
+
+func (ps *PngScorer) ScoreImage(ctx context.Context, img image.Image) (float64, error) {
+	buf := &discardCounter{}
+
+	img256 := downSampleImage(img)
+
+	err := ps.enc.Encode(buf, img256)
+	if err != nil {
+		return 0, err
+	}
+
+	origSize, err := uncompressedImageSize(img)
+	if err != nil {
+		return 0, err
+	}
+	return float64(buf.count) / origSize, nil
+}
+
+type singleThreadBufferPool png.EncoderBuffer
+
+var _ png.EncoderBufferPool = (*singleThreadBufferPool)(nil)
+
+func (bp *singleThreadBufferPool) Get() *png.EncoderBuffer {
+	return (*png.EncoderBuffer)(bp)
+}
+func (bp *singleThreadBufferPool) Put(eb *png.EncoderBuffer) {}
 
 type bufferPool sync.Pool
 
 var _ png.EncoderBufferPool = (*bufferPool)(nil)
 
-var bp *bufferPool = (*bufferPool)(&sync.Pool{
+var sharedBufferPool *bufferPool = (*bufferPool)(&sync.Pool{
 	New: func() any {
 		return &png.EncoderBuffer{}
 	},
@@ -28,26 +63,4 @@ func (bp *bufferPool) Get() *png.EncoderBuffer {
 }
 func (bp *bufferPool) Put(eb *png.EncoderBuffer) {
 	(*sync.Pool)(bp).Put(eb)
-}
-
-func (ps *PngScorer) ScoreImage(ctx context.Context, img image.Image) (float64, error) {
-	enc := png.Encoder{
-		CompressionLevel: png.BestSpeed,
-		BufferPool:       bp,
-	}
-
-	buf := &discardCounter{}
-
-	img256 := downSampleImage(img)
-
-	err := enc.Encode(buf, img256)
-	if err != nil {
-		return 0, err
-	}
-
-	origSize, err := uncompressedImageSize(img)
-	if err != nil {
-		return 0, err
-	}
-	return float64(buf.count) / origSize, nil
 }
